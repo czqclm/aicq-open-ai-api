34636c1f1cdb7ad7106f039e515186280fdd14b1
czqclm <1529718291@qq.com>
2023-04-12 11:25:30 +0800

    fix: usage json format

--- src/main/java/icu/aicq/ai/open/ai/api/pojo/dto/OpenAIUsageDTO.java
+++ src/main/java/icu/aicq/ai/open/ai/api/pojo/dto/OpenAIUsageDTO.java
@@ -1,5 +1,6 @@
 package icu.aicq.ai.open.ai.api.pojo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +16,12 @@ import lombok.NoArgsConstructor;
 @Builder
 public class OpenAIUsageDTO {
 
+    @JsonProperty("prompt_tokens")
     private Integer promptTokens;
 
+    @JsonProperty("completion_tokens")
     private Integer completionTokens;
 
+    @JsonProperty("total_tokens")
     private Integer totalTokens;
 }
