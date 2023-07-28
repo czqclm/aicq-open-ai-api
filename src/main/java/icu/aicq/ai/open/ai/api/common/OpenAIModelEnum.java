9db309449f15ed35d6e80009acccab855738627c
zhiqi <zhiqi@ideamake.cn>
2023-07-28 11:49:24 +0800

    feat: 支持 gpt-4

--- src/main/java/icu/aicq/ai/open/ai/api/common/OpenAIModelEnum.java
+++ src/main/java/icu/aicq/ai/open/ai/api/common/OpenAIModelEnum.java
@@ -14,7 +14,9 @@ public enum OpenAIModelEnum {
     /**
      * chatGPT
      */
-    GPT_3_5_TURBO("gpt-3.5-turbo");
+    GPT_3_5_TURBO("gpt-3.5-turbo"),
+
+    GPT_4("gpt-4");
 
     private final String code;
 }
